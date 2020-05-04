976ab6e809ccb11b65e7e0f7d4bc1b73426e54d4
Antoine Bantzhaff <antoine@tico.lan>
2020-05-04 21:57:01 +1100

    feat: myfox API model

---
+++ src/model/myfox-api/device.ts
@@ -0,0 +1,5 @@
+export interface Device{
+  deviceId: string;
+  label: string;
+  modelLabel: string;
+}
---
+++ src/model/myfox-api/group.ts
@@ -0,0 +1,8 @@
+import { Device } from "./device";
+
+export interface Group{
+  groupId: string;
+  label: string;
+  type: string;
+  devices: Device[];
+}
